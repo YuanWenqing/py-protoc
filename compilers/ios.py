1a26c7402d0cf0b911c865539dac27d54022e82f
yuan <yuan@yzyw-inc.com>
2017-02-07 20:28:30 +0800

    MOD: little mod

--- compilers/ios.py
+++ compilers/ios.py
@@ -3,9 +3,11 @@
 from base import *
 from writer import *
 
-class IosCompiler(Compiler):
+class IosHCompiler(Compiler):
   pass
 
+class IosMCompiler(Compiler):
+  pass
 
 class IosResolver(TypeResolver):
   pass
