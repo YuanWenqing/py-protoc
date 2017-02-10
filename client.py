2fe158d67950d8be0271a07670be0198aeb47cc9
yuan <yuan@yzyw-inc.com>
2017-02-10 14:58:09 +0800

    MOD: class client

--- client.py
+++ client.py
@@ -14,53 +14,58 @@ from compilers.android import *
 from compilers.ios import *
 from compilers.typescript import *
 
-def config(conf_file):
-  path_root = os.path.dirname(os.path.abspath(conf_file))
-  cf = ConfigParser.ConfigParser()
-  cf.read(conf_file)
-  proto_dir = cf.get('proto', 'proto_dir')
-  proto_dir = os.path.join(path_root, proto_dir)
-  loader = Loader(proto_dir)
-  out_root = cf.get('proto', 'out_root')
-  out_root = os.path.join(path_root, out_root)
+class ClientCompiler:
+  def __init__(self, conf_file):
+    self.conf_file = conf_file
+    self.path_root = os.path.dirname(os.path.abspath(conf_file))
+    self.cf = ConfigParser.ConfigParser()
+    self.cf.read(conf_file)
+    proto_dir = self.cf.get('proto', 'proto_dir')
+    proto_dir = os.path.join(self.path_root, proto_dir)
+    self.loader = Loader(proto_dir)
+    out_root = self.cf.get('proto', 'out_root')
+    self.out_root = os.path.join(self.path_root, out_root)
 
-  compilers = []
+    self.compilers = []
 
-  # android
-  out_dir = os.path.join(out_root, 'android')
-  files = []
-  for f in cf.get('android', 'input_proto').split(','):
-    f = os.path.join(path_root, proto_dir)
-    files.append(f)
-  resolver = AndroidResolver()
-  writer = AndroidWriter(out_dir, '.java')
-  compiler = AndroidCompiler(loader, writer, resolver)
-  compilers.append((compiler, files))
-  # ios
-  out_dir = os.path.join(out_root, 'ios')
-  files = []
-  for f in cf.get('ios', 'input_proto').split(','):
-    f = os.path.join(path_root, proto_dir)
-    files.append(f)
-  resolver = IosResolver()
-  writer = IosWriter(out_dir, '.h')
-  compiler = IosHCompiler(loader, writer, resolver)
-  compilers.append((compiler, files))
-  writer = IosWriter(out_dir, '.m')
-  compiler = IosMCompiler(loader, writer, resolver)
-  compilers.append((compiler, files))
-  # typescript
-  out_dir = os.path.join(out_root, 'typescript')
-  files = []
-  for f in cf.get('typescript', 'input_proto').split(','):
-    f = os.path.join(path_root, proto_dir)
-    files.append(f)
-  resolver = TypeScriptResolver()
-  writer = TypeScriptWriter(out_dir, '.ts')
-  compiler = TypeScriptCompiler(loader, writer, resolver)
-  compilers.append((compiler, files))
+  def __getInputProtos(self, section):
+    files = []
+    for f in self.cf.get(section, 'input_proto').split(','):
+      f = os.path.join(self.path_root, f)
+      if f not in files:
+        files.append(f)
+    return files
 
-  return compilers
+  def config(self):
+    # android
+    out_dir = os.path.join(self.out_root, 'android')
+    files = self.__getInputProtos('android')
+    resolver = AndroidResolver()
+    writer = AndroidWriter(out_dir, '.java')
+    compiler = AndroidCompiler(self.loader, writer, resolver)
+    self.compilers.append((compiler, files))
+    # ios
+    out_dir = os.path.join(self.out_root, 'ios')
+    files = self.__getInputProtos('ios')
+    resolver = IosResolver()
+    writer = IosWriter(out_dir, '.h')
+    compiler = IosHCompiler(self.loader, writer, resolver)
+    self.compilers.append((compiler, files))
+    writer = IosWriter(out_dir, '.m')
+    compiler = IosMCompiler(self.loader, writer, resolver)
+    self.compilers.append((compiler, files))
+    # typescript
+    out_dir = os.path.join(self.out_root, 'typescript')
+    files = self.__getInputProtos('typescript')
+    resolver = TypeScriptResolver()
+    writer = TypeScriptWriter(out_dir, '.ts')
+    compiler = TypeScriptCompiler(self.loader, writer, resolver)
+    self.compilers.append((compiler, files))
+
+  def do(self):
+    for compiler, files in self.compilers:
+      print '> run %s' % compiler.__class__.__name__
+      compiler.compile(files)
 
 if __name__ == '__main__':
   optParser = OptionParser()
@@ -72,7 +77,6 @@ if __name__ == '__main__':
     optParser.print_help()
     optParser.error('no conf file')
 
-  compilers = config(options.conf_file)
-  for compiler, files in compilers:
-    print '> run %s' % compiler.__class__.__name__
-    compiler.compile(files)
+  cc = ClientCompiler(options.conf_file)
+  cc.config()
+  cc.do()
